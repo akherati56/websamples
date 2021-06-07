79c231662dd297c915f68abfb24dc158a4c4efea
akherati56 <akherati5660@gmail.com>
2021-06-08 00:07:38 +0430

    update
    update for my own use

--- python_send_file/client.py
+++ python_send_file/client.py
@@ -4,19 +4,22 @@
 import socket
 import sys
 
+from config import PORT , ServerIp
+
 # Lets catch the 1st argument as server ip
-if (len(sys.argv) > 1):
-    ServerIp = sys.argv[1]
-else:
-    print("\n\n Run like \n python3 client.py < serverip address > \n\n")
-    exit(1)
+
+
+#if (len(sys.argv) > 1):
+#    ServerIp = sys.argv[1]
+#else:
+#    print("\n\n Run like \n python3 client.py < serverip address > \n\n")
+#    exit(1)
 
 
 # Now we can create socket object
 s = socket.socket()
 
 # Lets choose one port and connect to that port
-PORT = 9898
 
 # Lets connect to that port where server may be running
 s.connect((ServerIp, PORT))
@@ -31,9 +34,7 @@ while SendData:
     print("\n\n################## Below message is received from server ################## \n\n ", s.recv(1024).decode("utf-8"))
     #Now send the content of sample.txt to server
     s.send(SendData)
-    SendData = file.read(1024)      
+    SendData = file.read(1024)
 
 # Close the connection from client side
 s.close()
-
-
--- python_send_file/server.py
+++ python_send_file/server.py
@@ -1,34 +1,35 @@
 #!/usr/bin/env python3
 
-# Importing socket library 
+# Importing socket library
 import socket
+from config import PORT , ServerIp
 
 # Now we can create socket object
 s = socket.socket()
 
 # Lets choose one port and start listening on that port
-PORT = 9898
+
 print("\n Server is listing on port :", PORT, "\n")
 
 # Now we need to bind to the above port at server side
-s.bind(('', PORT))
+s.bind(('172.20.10.4', PORT))
 
-# Now we will put server into listenig  mode 
+# Now we will put server into listenig  mode
 s.listen(10)
 
 #Open one recv.txt file in write mode
-file = open("recv.txt", "wb") 
+file = open("recv.txt", "wb")
 print("\n Copied file name will be recv.txt at server side\n")
 
-# Now we do not know when client will concatct server so server should be listening contineously  
+# Now we do not know when client will concatct server so server should be listening contineously
 while True:
     # Now we can establish connection with clien
     conn, addr = s.accept()
 
     # Send a hello message to client
-    msg = "\n\n|---------------------------------|\n Hi Client[IP address: "+ addr[0] + "], \n ֲֳ**Welcome to Server** \n -Server\n|---------------------------------|\n \n\n"    
+    msg = "\n\n|---------------------------------|\n Hi Client[IP address: "+ addr[0] + "], \n ֲֳ**Welcome to Server** \n -Server\n|---------------------------------|\n \n\n"
     conn.send(msg.encode())
-    
+
     # Receive any data from client side
     RecvData = conn.recv(1024)
     while RecvData:
@@ -45,4 +46,3 @@ while True:
 
     # Come out from the infinite while loop as the file has been copied from client.
     break
-
